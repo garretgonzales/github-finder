485031203aaf46a14f2e24aa0a39e10f4c3bf0df
garretgonzales <garret915@gmail.com>
2020-08-12 16:08:58 -0500

    Bug with fetching Repos :anger:

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -38,6 +39,17 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get User Repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  };
+
   // Get a single Github user
   getUser = async (username) => {
     this.setState({ loading: true });
@@ -60,7 +72,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
 
     return (
       <Router>
@@ -92,7 +104,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.props.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
